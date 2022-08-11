c8400581cd13636a45d50b3f96d19bf01dad8931
Aldo Nunes <aldonunes001@gmail.com>
2022-08-11 17:37:33 -0300

    Criando validações dos formulários com wtforms.validators

--- forms.py
+++ forms.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class FormCriarConta(FlaskForm):
-    username = StringField('Nome de Usuário')
-    email = StringField('E-mail')
-    senha = PasswordField('Senha')
-    confirmacao = PasswordField('Confirmação da Senha')
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail')
-    senha = PasswordField('Senha')
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     botao_submit_login = SubmitField('Fazer Login')
