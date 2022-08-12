991e096d5dadd0258544f100679ca30f25be63e4
Aldo Nunes <aldonunes001@gmail.com>
2022-08-11 22:11:15 -0300

    Criando CSRF Token

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
+app.config['SECRET_KEY'] = '67049ce9faacc9f10c352d4bfb70cdbb'  # utilizei o secrets.token_hex(16)
+
 
 @app.route('/')
 def home():
