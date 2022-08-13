3221124cd8d377f66583e2ef6c9fea90f3b67caa
Aldo Nunes <aldonunes001@gmail.com>
2022-08-13 10:54:53 -0300

    Bug de repetição de email de login no campo de criarconta consertado

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ def login():
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
         # Fez login com sucesso
-        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        flash(f'Login feito com sucesso no e-mail: {form_login.email_login.data}', 'alert-success')
         return redirect(url_for('home'))
 
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         # Criou conta com sucesso
-        flash(f'Conta criada com sucesso para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        flash(f'Conta criada com sucesso para o e-mail: {form_criarconta.email_criarconta.data}', 'alert-success')
         return redirect(url_for('home'))
 
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
