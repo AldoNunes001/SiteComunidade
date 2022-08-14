7304341c5c761022531f2bafc03f54dec838dfae
Aldo Nunes <aldonunes001@gmail.com>
2022-08-14 16:12:43 -0300

    Implementando função de login no site

--- sitecomunidade/__init__.py
+++ sitecomunidade/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ app.config['SECRET_KEY'] = '67049ce9faacc9f10c352d4bfb70cdbb'  # utilizei o secr
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db?charset=utf8'
 
 database = SQLAlchemy(app)
-
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from sitecomunidade import routes
--- sitecomunidade/routes.py
+++ sitecomunidade/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for, flash
 from sitecomunidade import app, database, bcrypt
 from sitecomunidade.forms import FormLogin, FormCriarConta
 from sitecomunidade.models import Usuario
+from flask_login import login_user
 
 lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
@@ -28,13 +29,18 @@ def login():
 
     if 'botao_submit_login' in request.form:
         if form_login.validate_on_submit():
-            # Fez login com sucesso
-            flash(f'Login feito com sucesso no e-mail: {form_login.email_login.data}', 'alert-success')
-            return redirect(url_for('home'))
+            usuario = Usuario.query.filter_by(email=form_login.email_login.data).first()
+            if usuario and bcrypt.check_password_hash(usuario.senha.encode('utf-8'), form_login.senha.data):
+                login_user(usuario, remember=form_login.lembrar_dados.data)
+                # Fez login com sucesso
+                flash(f'Login feito com sucesso no e-mail: {form_login.email_login.data}', 'alert-success')
+                return redirect(url_for('home'))
+            else:
+                flash(f'Falha no login. E-mail ou senha incorretos.', 'alert-danger')
 
     if 'botao_submit_criarconta' in request.form:
         if form_criarconta.validate_on_submit():
-            senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+            senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data).decode('utf-8')
             usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email_criarconta.data,
                               senha=senha_cript)
             database.session.add(usuario)
