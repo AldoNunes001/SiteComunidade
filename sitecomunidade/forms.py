9ee05f38db6fa27090e04733a3108afbf0cd0f90
Aldo Nunes <aldonunes001@gmail.com>
2022-08-14 15:34:51 -0300

    Implementando validação para não permitir criar mais de 1 usuário com o mesmo email

--- sitecomunidade/forms.py
+++ sitecomunidade/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from sitecomunidade.models import Usuario
 
 
 class FormCriarConta(FlaskForm):
@@ -9,6 +10,14 @@ class FormCriarConta(FlaskForm):
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
+
+    # A função 'validate_on_submit' do FlaskForm executa automaticamente qualquer função que começa com 'validate_'
+    # O nome da função tem que começar com 'validate_' + o nome da variável para validar
+    # Estava tentando usar a função com o nome 'validate_email' e estava dando erro
+    def validate_email_criarconta(self, email_criarconta):
+        usuario = Usuario.query.filter_by(email=email_criarconta.data).first()
+        if usuario:
+            raise ValidationError('E-mail já cadastrado. Cadastra-se com outro e-mail ou faça login para continuar.')
 
 
 class FormLogin(FlaskForm):
