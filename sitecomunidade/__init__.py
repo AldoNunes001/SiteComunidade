80104f77d4dd3537e34049532d1a97d78636d330
Aldo Nunes <aldonunes001@gmail.com>
2022-08-14 14:25:44 -0300

    Definindo charset utf8 para o DB

--- sitecomunidade/__init__.py
+++ sitecomunidade/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '67049ce9faacc9f10c352d4bfb70cdbb'  # utilizei o secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db?charset=utf8'
 
 database = SQLAlchemy(app)
 
